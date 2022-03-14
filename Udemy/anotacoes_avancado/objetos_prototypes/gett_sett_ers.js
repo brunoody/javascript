861c326f3974670d694bafc44b465b41937fc086
MarcToller <marcelo.toller@yahoo.com.br>
2022-03-14 13:22:56 -0300

    getters e setters

--- Udemy/anotacoes_avancado/objetos_prototypes/gett_sett_ers.js
+++ Udemy/anotacoes_avancado/objetos_prototypes/gett_sett_ers.js
@@ -2,18 +2,31 @@
 
 
 // criei o nome do arquivo assim pois não deixou criar getters_setters
+// Get e Set similar ao Delphi nas propriedades
 
 function Produto (nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
 
+    // pode-se criar uma variavel "private" para trabalhar com set e get igual nas propriedades do Delphi:
+    let estoquePrivado = estoque;
     Object.defineProperty(this, 
                           'estoque', 
                           {
                               enumerable: true,
-                              value: estoque, 
-                              writable: false,
-                              configurable : false 
+                              //value: estoque, com getter e setter esses 2 atributos podem ser eliminados 
+                              //writable: false,
+                              configurable : false, 
+                              get: function () {
+                                  return estoquePrivado;
+                              },
+                              set: function(valor) {
+                                   // validação no getter 
+                                  if (typeof valor !== 'number') {
+                                      throw new TypeError('não é um numero');
+                                  }
+                                  estoquePrivado = valor;
+                              }
                           });                  
 } 
 
