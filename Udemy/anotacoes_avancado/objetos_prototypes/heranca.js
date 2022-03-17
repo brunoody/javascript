2a199153a10fe80f6a28159ba51690b25bc541e0
MarcToller <marcelo.toller@yahoo.com.br>
2022-03-17 07:48:23 -0300

    herança

--- Udemy/anotacoes_avancado/objetos_prototypes/heranca.js
+++ Udemy/anotacoes_avancado/objetos_prototypes/heranca.js
@@ -1,9 +1,9 @@
 
+// criando o Produto - pai
 function Produto (nome, preco) {
     this.nome = nome;
     this.preco = preco;
 };
-
 Produto.prototype.desconto = function(quantia) {
     this.preco -= quantia;
 }
@@ -11,6 +11,7 @@ Produto.prototype.aumento = function(quantia) {
     this.preco += quantia;
 }
 
+//criando o filho Camiseta do Produto
 function Camiseta (nome, preco, cor) {
     Produto.call(this, nome, preco); // isso é a "herença" bem parecido com delphi
     this.cor = cor;        
@@ -19,7 +20,40 @@ function Camiseta (nome, preco, cor) {
 Camiseta.prototype = Object.create(Produto.prototype);
 // porem o construtor de camiseta ainda vai aparecer como produto, então temos que ajustar isso tb:
 Camiseta.prototype.constructor = Camiseta;
+// sobrescrevendo o método aumento: Não "herda" o resultado da função pai como no Delphi, ele sobrescreve mesmo!
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual/100));
+}
 
+//criando o filho Caneca do Produto
+function Caneca(nome, preco, material){
+    Produto.call(this, nome, preco);
+    this.material = material;
+    Object.defineProperty(this, 'estoque', {
+        get: function() {
+            return estoque;
+        } ,
+        set: function(valor){
+            if (typeof valor === 'number'){
+                estoque = valor;
+            }              
+        },
+
+        enumerable: true,
+        configurable: false        
+    })
+}
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+// criando os objetos
+const produto = new Produto('Produto', 10);
 const camiseta = new Camiseta('Regata', 50, 'Preta');
-camiseta.aumento(10);
+const caneca = new Caneca('caneca', 20, 'Plastico');
+caneca.estoque = 100;
+
+
+console.log(produto);
 console.log(camiseta);
+console.log(caneca);
+
