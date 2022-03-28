5a466c3e0a4216f11077107c31510906c9b933f4
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-03-28 20:25:06 -0300

    Módulos

---
+++ Udemy/javascript_modulos/index.js
@@ -0,0 +1,6 @@
+
+import  {numero1, objeto, soma} from './modulo1.js';
+
+const resultado = soma(1,2);
+
+console.log(resultado)
---
+++ Udemy/javascript_modulos/modulo1.js
@@ -0,0 +1,14 @@
+
+const numero1 = 10;
+const numero2 = 20;
+let objeto = {
+    nome: 'Marcelo',
+    sobrenome: 'Toller'
+};
+
+function soma(x,y) {
+    return x + y;
+}
+
+export {numero1, objeto, soma}
+
