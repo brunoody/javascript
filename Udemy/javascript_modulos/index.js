9234edde7b274b10efd71feaa0d61b4753093374
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-03-29 08:16:11 -0300

    modulos

--- Udemy/javascript_modulos/index.js
+++ Udemy/javascript_modulos/index.js
@@ -1,6 +1,9 @@
 
+
+
+// tem que executar pelo console do chrome!! e no html, na chamada no script, tem que informar que o type="module"
 import  {numero1, objeto, soma} from './modulo1.js';
 
 const resultado = soma(1,2);
 
-console.log(resultado)
+console.log('soma:', resultado, numero1, objeto);
