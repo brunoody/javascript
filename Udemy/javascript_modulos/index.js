377769dbed94b276315999ca2a295c42af96c3b1
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-03-29 13:29:11 -0300

    modulos

--- Udemy/javascript_modulos/index.js
+++ Udemy/javascript_modulos/index.js
@@ -1,9 +1,33 @@
+////////////////////////import default e outros:
+import MinhafuncaoDefault, {soma, numero1, numero3}  from './modulo1.js';
 
 
+////////////////////////import default: Posso eleger apenas uma função, constante, variavel, classe.. para ser a defaul:
+import MinhafuncaoDefault from './modulo1.js';
+console.log(MinhafuncaoDefault);
 
+
+//////////////////////// posso importar tudo de uma só vez e essa importação coloca tudo dentro de um objeto: 
+import * as MeuModulo from './modulo1.js';
+
+console.log(MeuModulo);
+console.log(MeuModulo.soma(1000,2151));
+
+//////////////////////// importar somente o que precisar: 
 // tem que executar pelo console do chrome!! e no html, na chamada no script, tem que informar que o type="module"
-import  {numero1, objeto, soma} from './modulo1.js';
+import  {numero1 as numeroImport , objetoExport, soma, multiplica, Pessoa as PessoaImport} from './modulo1.js';
 
-const resultado = soma(1,2);
+// se eu já tiver uma constante, variavel, objeto com o mesmo nome vai dar erro. Para resolver isso podemos renomear no próprio import acima:
+// posso tb já nomar no export!!
+const numero1 = 1;
 
-console.log('soma:', resultado, numero1, objeto);
+const objeto = {
+    nome: 'Marcelo2',
+    sobrenome: 'Toller2'
+}
+
+const resultadoSoma = soma(1,2);
+const classePessoaImport = new PessoaImport('Marcelo3', 'Toller3');
+
+console.log(resultadoSoma, numero1, numeroImport, objeto, objetoExport, multiplica(3,9));
+console.log(classePessoaImport)
--- Udemy/javascript_modulos/modulo1.js
+++ Udemy/javascript_modulos/modulo1.js
@@ -2,13 +2,35 @@
 const numero1 = 10;
 const numero2 = 20;
 let objeto = {
-    nome: 'Marcelo',
-    sobrenome: 'Toller'
+    nome: 'Marcelo1',
+    sobrenome: 'Toller1'
 };
 
 function soma(x,y) {
     return x + y;
 }
 
-export {numero1, objeto, soma}
+export {numero1, objeto as objetoExport, soma}
+
+// posso criar e já exportar, posso continuar usando aqui nesta unit:
+export const numero3 = 3;
+export function multiplica(x,y) {
+    return x*y;
+}
+
+export class Pessoa {
+    constructor(nome, sobrenome ){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+// só posso exportar uma unica coisa como default
+export default function funcaoDefault () {
+    return 'funcaoDefault';
+}
+
+// para poder exportar uma constante como default:
+//const teste = 'teste';
+//export {teste as defaul}
 
