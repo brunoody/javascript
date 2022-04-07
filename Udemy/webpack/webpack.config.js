3d971b237b966ee6c219e8c09f04548d4c4fccea
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-04-06 21:12:01 -0300

    webpack

--- Udemy/webpack/webpack.config.js
+++ Udemy/webpack/webpack.config.js
@@ -8,5 +8,12 @@ module.exports = {
     output: { // é um objeto
         path: path.resolve(__dirname, 'src', 'assets', 'js'), // nesta pasta vai ser gerado o arquivo bunble que contem o código js compativel com navegadores mais antigos
         filename: 'bundle.js'
-    }   
+    },
+    module: {
+        rules: [{ // array de objetos, no nosso caso só vamos ter um pois só vamos trabalhar com JS
+            exclude: /node_modules/, // para não ficar analizando a pasta node_modules, é uma expressão regular
+            test: /\.js$/ // expressão regular, test é para considerar os arquivos com extensão JS para formar o bundle
+        }]
+    },
+    devtool: 'source-map' // esse cara permite que o navegador mostre qual o fonte (moderno) deu algum erro, pois o bondle é um unico arquivo completamemte diferente e ainda por cima engloba vários outros arquivos, então é impossivel encontrar um erro nele sendo que ele é gerado automaticamente 
 }
