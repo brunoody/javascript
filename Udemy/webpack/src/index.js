ed3bca2144dfdb46d15daf86123e602f984635b0
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-04-07 12:57:27 -0300

    webpack

---
+++ Udemy/webpack/src/index.js
@@ -0,0 +1,2 @@
+alert('olá mundo');
+console.log('teste'); // para olhar no console do navegador quando abrir a index.html para propvar que ele mostra alguma fgalha ou ação neste arquivo original mesmo que na index.html esteja linkada somente a bundle.js
