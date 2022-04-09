21831ee422868fb9fd71e326ebcb60c7a08d9fed
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-04-09 15:44:23 -0300

    express com webpack

--- Udemy/node/express/express_webpack/src/controllers/homeController.js
+++ Udemy/node/express/express_webpack/src/controllers/homeController.js
@@ -1,5 +1,5 @@
 exports.paginaInicial = (req, res) => {
-    res.render('index');
+    res.render('index');// reinderiza (transforma) o index.ejs e mostra na tela um "html" 
 }
 
 exports.trataPost = (req, res) => {
