0af288dac9593e5e146581ad2bcd3a2702b891bd
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-04-13 08:23:25 -0300

    Ajustes nas aula de revisão

--- Udemy/node/express/helmet_csrf/server.js
+++ Udemy/node/express/helmet_csrf/server.js
@@ -3,12 +3,13 @@ require ('dotenv').config();// isso aqui é para proteger a minha url de conexã
 const express = require('express');
 const app = express();
 
+const alertaConexaoPronta = 'conexaoMongoPronta';
+
 //Com o dotenv não precisamos desta constante de sstring de conexão
 //const connectionString = 'mongodb+srv://marcelotoller:78174949Mt@cursojs01.x2s5l.mongodb.net/meuPrimeiroBancoMongoDb?retryWrites=true&w=majority';
 const mongosse = require('mongoose');
 
-const alertaConexaoPronta = 'conexaoMongoPronta';
-
+//################ conexão com mongodb
 //Este connect  retorna uma primisse!!!
 // aqui em vez de usar a constante de conexão comentada acima, usamo o valor do arquivo .env:
 mongosse.connect(process.env.CONNECTIONSTRING, { 
@@ -20,31 +21,33 @@ mongosse.connect(process.env.CONNECTIONSTRING, {
     app.emit(alertaConexaoPronta)})
  .catch((e) => console.log(e)); // neste catch poderia mandar ele patra outra página, outra rota por exemplo;
 // A conexão acima começa depois de o nosso servidor começar a "escutar" e precisamos que isso ocorra antes, então dentro do then da promessa da conexão vamos colocar um comando do nosso app emitir um sinal de quando estiver pronto para conectar! app.emit. Lá em baixo, no app.listen pegamos esta notificação e seguimos com nosso servidor.
-
+//###########################
 
 //################ session
 const minhaSession = require('express-session');
-const MongoStore = require('connect-mongo'); 
+const MongoStore = require('connect-mongo'); // salvar as sessões na base de dados, por padrão é salvo na memória do servidor, mas isso consome muito recurso
 const flash = require('connect-flash');
-//################ session
+//###########################
 
-const routes = require('./routes.js')
+const routes = require('./routes.js'); //rotas: / (página inicial), /contato, /sobre, etc..
 const path = require('path');
-const helmet = require('helmet');
-const csrtf = require('csurf'); // ver middleware na pasta middlewares
 
+//################ segurança
+const helmet = require('helmet');// professor não deu detalhes, disse para pesquisar, mas ele faz algo relacionado à segurança nos cabeçalhos das páginas
+const csrtf = require('csurf'); // ver middleware na pasta middlewares, são tokens de segurança
+//###########################
+
+// importando middlewares
 const { meuMidlleware, checkCsrfError, csrfMiddleware } = require(path.resolve(__dirname, 'src', 'midllewares', 'midlleware.js'));
 
+// usando helmet
 app.use(helmet());
 
 // comando necessário para que o post receba os parametros pelo req. body
 app.use(express.urlencoded({extended: true}))
+app.use(express.json()); // faz o mesmo que a linha acima mas com json
 
-
-// esta é uma pasta aonde vai ficar meus conteudos estáticis
-// ela não é uma rota, não preciso digitar /public
-// como exemplo vou colocar um arquivo txt nela e 
-// no navegador acesso ele direto, assim: 127.0.0.1:3000/teste.txt
+// esta é uma pasta aonde vai ficar meus conteudos estaticos. Ela não é uma rota, não preciso digitar /public como exemplo vou colocar um arquivo txt nela e no navegador acesso ele direto, assim: 127.0.0.1:3000/teste.txt
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 // configurando a session
@@ -68,10 +71,16 @@ app.use(sessionOptions);
 // temos que dizer para o nosso app usar a nossa flash message. Ver arquivo anotações.txt
 app.use(flash());
 
+// o primeiro 'views' é o nome do recurso que eu quero usar, no caso, as views o src e views é o caminho de onde estão as minhas views e que estou deixando para o path.reesolve resolver p mim. 
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+
+// view engine: mesmo coisa: primeira string é o nome do recurso, e o segundo parametro é a engine que eu quero usar para reinderizar as views, tem vários, aqui vamos usar o ejs. E necessário instalar o ejs: terminal: npm i ejs
+app.set('view engine', 'ejs');
+
 // importante que esteja aqui, se colocar mais p cima não funciona
 app.use(csrtf());
 
-// midlle global. Descobri da pior forma que este use de Midlleware tem que vir DEPOIS da linha acima que é a que permite usar o body nos parametros req e res
+// midlle global. Descobri da pior forma que este use de Midlleware tem que vir DEPOIS da linha acima que é a que permite usar o body nos parametros req e res "app.use(express.urlencoded({extended: true}))""
 app.use(meuMidlleware);
 
 //middleware que captura erro de falta de token e mosytra uma página, a ordem em que ele é executado faz diferença, me quebrei com isso!
@@ -82,14 +91,6 @@ app.use(csrfMiddleware);
 
 // comando para o app usar o routes:
 app.use(routes);
-
-// o primeiro 'views' é o nome do recurso que eu quero usar, no caso, as views
-// o src e views é o caminho de onde estão as minhas views e que estou deixando para o path.reesolve resolver p mim
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-
-// view engine: mesmo coisa: primeira string é o nome do recurso, e o segundo parametro é a engine que eu quero usar para reinderizar as views, tem vários, aqui vamos usar o ejs
-// é necessário instalar o ejs: terminal: npm i ejs
-app.set('view engine', 'ejs');
 
 // aqui o app recebe uma notificação (string conexaoMongoPronta) e pode começar a "escutar"
 app.on(alertaConexaoPronta, () => {
