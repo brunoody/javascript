bc6a743446769f7894c7e8995ac45fc37dbbf1b8
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-04-17 13:06:54 -0300

    Projeto Agenda - Cadastrando usuários - Hash senha, validação se usuário já existe

--- Udemy/node/express/projeto_agenda/src/models/LoginModel.js
+++ Udemy/node/express/projeto_agenda/src/models/LoginModel.js
@@ -1,6 +1,7 @@
 
 const validator = require('validator');
 const mongosse = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 // modelo de estrutura que vai ser gravado na base de dados
 const LoginScheme = new mongosse.Schema({
@@ -17,6 +18,7 @@ class Login {
         this.errors = [];
         this.user = null;   
     }
+    
     valida() {
         this.cleanUp();
         // instalamos o pacote validator: npm i validator
@@ -28,13 +30,34 @@ class Login {
         // senha precisa ter entre 3 e 8 caracteres
         if (this.body.password.length < 3 || this.body.password.length > 8) {
             this.errors.push('A Senha deve ter entre 3 e 8 caracteres');
-        }        
+        };                
     }
+
+    // como vou lidar com base de dados tem que ser async!!
+    async usuariojaCadastrado() {         
+        if (this.errors.length === 0) {            
+            const user = await LoginModel.findOne({email: this.body.email});// manda um objeto que seria como se fosse um "where" na base, pesqiisando na "tabela" de login pelo "campo" email com o valor que o usuário digitou;
+
+            //se  a const User vier com valor é pq ele já exite:
+            if (user) {            
+                this.errors.push('Usuário já existe');
+            }
+        } 
+    }
+
     // esse register precisa ser async pq vai executar um registro na base de dados e retornar uma promisse!
     // Como esta função é async, o ponto aonde ela é chamada tb deve ser!! (vr loginController)
     async register () {
         this.valida();
+
+        // chamada await pq ela é async devido a lidar com banco de dados
+        await this.usuariojaCadastrado(); 
+                
         if (this.errors.length > 0) return; // para parar
+
+        // fazendo um hash da senha (criptografando) pacote: npm i bcryptjs        
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt) ;
 
         try {
             this.user = await LoginModel.create(this.body);// isso é uma promisse, aqui o body já foi validado e removidas as chaves que eu não quero mandar para a base (método cleanUp)
