35697eebe30301c74bd10bf6984711b14999dfef
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-04-19 22:18:02 -0300

    Projeto Agenda - Formulario de cadastro de contato

--- Udemy/node/express/projeto_agenda/src/controllers/contatoController.js
+++ Udemy/node/express/projeto_agenda/src/controllers/contatoController.js
@@ -1,8 +1,26 @@
-exports.index = (req, res) => {        
+const Contato = require('../models/ContatoModel.js');
+
+exports.index = (req, res) => {       
     res.render('contato');    
 }
 
-exports.register = (req, res) => {
-    //res.send(req.body);
-} 
+exports.register = async (req, res) => {
+    try {
+    // aqui p body passa por dentro da classe e são feitas validações
+        const contato = new Contato(req.body);
+        await contato.register();
 
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);                                
+            
+        } else {
+            req.flash('sucess', 'Contato cadastrado com sucesso.'); 
+        };  
+
+        res.redirect('/contato/index');                             
+
+    } catch (e) {
+        console.log(e);
+        res.render('404.ejs')
+    }
+}
--- Udemy/node/express/projeto_agenda/src/models/ContatoModel.js
+++ Udemy/node/express/projeto_agenda/src/models/ContatoModel.js
@@ -9,6 +9,63 @@ const ContatoScheme = new mongosse.Schema({
 
 // criação do model:
 const ContatoModel = mongosse.model('Contato', ContatoScheme); // nome e eschema do model
+const validator = require('validator');
+
+class Contato {
+    constructor(body) {
+        this.body = body;
+        this.errors = [];
+        this.Contato = null;   
+    }
+    
+    valida() {
+        this.cleanUp();
+
+        console.log('telefone: ', this.body.telefone, typeof this.body.telefone);
+        if (!validator.isEmail(this.body.email)) {
+            this.errors.push('E-mail inválido');
+        };
+    }
+    
+    async contatojaCadastrado () {  
+        
+        if (this.errors.length === 0) {            
+            this.contato = await ContatoModel.findOne({email: this.body.email});
+
+            if (this.contato) {            
+               this.errors.push('Contato com este e-mail já cadastrado');            
+            } 
+        }
+    }   
+    
+    async register () {
+        this.valida();
+
+        await this.contatojaCadastrado(); 
+                
+        if (this.errors.length > 0) return;
+        
+        this.contato = await ContatoModel.create(this.body);
+    }
+
+    cleanUp() {
+        // especie de validação (professor) que verifica se os valores das chaves que estão vindo do formuláro são do tipo string, se não for ele atribui uma string vazia ao valor.
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
+                this.body[key] = '' ;
+            }
+        }
+        // garantindo que o objeto vai ter somente os campos que preciso para enviar para a base, neste caso não presiamos no _csrf que vem automaticamente do form
+        this.body = {
+            nome: this.body.nome,
+            sobrenome: this.body.sobrenome,
+            email: this.body.email,
+            telefone: this.body.telefone        
+        }
+    }
+}
+
+
 
 // importado no ContatoController
-module.exports = ContatoModel;
+module.exports = Contato;
