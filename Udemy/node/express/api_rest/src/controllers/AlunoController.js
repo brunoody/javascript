5276c0e49a7756521fcd943ef95e54767f758fc5
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-06-30 21:27:33 -0300

    Api rest

--- Udemy/node/express/api_rest/src/controllers/AlunoController.js
+++ Udemy/node/express/api_rest/src/controllers/AlunoController.js
@@ -1,4 +1,5 @@
 // o nome do arquivo é com letra maiúscula pois vamos trabalhar com classes e classes começam com letras maiusculas.
+// import { Op } from 'sequelize';
 import Aluno from '../models/Aluno';
 
 class AlunoController {
@@ -9,6 +10,13 @@ class AlunoController {
 
   async index(req, res) {
     try {
+      // abaixo um exemplo de select between com query paremetros vindos da requisição!! Aprendi sozinho, professor não ensinou nada disso. Filtrei só os alunos com idade entre 42 e 43 anos, funcionou!
+      // no insomnia usei a seguinte url: https://localhost:3001/alunos/?email=marcelo@yahoo.com.br&idadeini=42&idadefim=43
+      // const { email, idadeini, idadefim } = req.query;
+      // console.log(email, idadeini, idadefim);
+      // const alunos = await Aluno.findAll(({ where: { idade: { [Op.between]: [idadeini, idadefim] } } }));
+      // usar o    import { Op } from 'sequelize';
+
       const alunos = await Aluno.findAll();
       return res.json(alunos);
     } catch {
