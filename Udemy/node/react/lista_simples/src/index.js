add1377319c30d0b628d47e761051ebe5f0c1af1
Marcelo Toller <marcelo.toller@yahoo.com.br>
2022-09-06 13:25:04 -0300

    React - Lista Simples

--- Udemy/node/react/lista_simples/src/index.js
+++ Udemy/node/react/lista_simples/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
-);
+); // este App é um sjx, ou seja, um tipo de html que se pode escrever JS junto
